ada37e098ae110225944d6d776242dc3d9bfff1a
ultrafluff <ultrafluffzander@gmail.com>
2024-01-20 11:25:44 -0500

    linked the js file and made the prompt password

--- javascript.js
+++ javascript.js
@@ -1,2 +1,25 @@
+let message = prompt("hello, please enter your password that zander gave you")
+
+switch(message) {
+    case 'S293821':
+    break;
+
+    case 'M596921':
+    break;
+
+    case 'F381928':
+    break;
+
+    case 'Z493819':
+    break;
+
+    case 'K283726':
+    break;
+
+    case 'V584923':
+    break;
+
+    default:
+}
 
 
